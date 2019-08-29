534c98090a266df8e36d89f0a0e33873e94a85f8
Abiye Chris. I. Surulere <suruabiye@gmail.com>
2019-08-29 15:51:24 +0100

    Change bind parameter of getUserByEmail to string

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function getUserByEmail($email,$dbConn){
     $stmt = $dbConn->prepare('select username from users where email=?');
-    $stmt->bind_param('i', $email);
+    $stmt->bind_param('s', $email);
     $stmt->execute();
     $stmt->bind_result($username);
     $stmt->store_result();
